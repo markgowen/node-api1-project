5769cfbdba137dabbf342f6f5740c5a26692500a
Mark Gowen <mark.gowen78@gmail.com>
2019-10-28 13:10:29 -0700

    updated index.js with correct params
    updated index.js with params that are relevant to the project.

--- index.js
+++ index.js
@@ -1,52 +1,70 @@
 // implement your API here
 const express = require('express');
 
+const db = require('./data/db.js')
+
 const server = express();
 
-// GET to /hubs that returns a list of hubs
-server.get('/hubs', (req, res) => {
+server.use(express.json());
+
+// GET to /users that returns a list of users
+server.get('/users', (req, res) => {
   db.find()
-    .then(hubs => {
-      res.status(200).json(hubs);
+    .then(data => {
+      res.status(200).json(data);
     })
     .catch(err => {
       console.log('error', err);
       res.status(500).json({
-        error: 'failed to get hubs from db'
+        error: 'failed to GET users from db'
+      });
+    });
+});
+
+// Get to /users that returns a user by id
+server.get('/users/:id', (req, res) => {
+  db.findById(id)
+    .then(user => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      console.log('error', err);
+      res.status(500).json({
+        error: `failed to GET user with ${id} from db`
       });
     });
 });
 
 // POST to /hubs
-server.post('/hubs', (req, res) => {
-  const hubInformation = req.body;
+server.post('/users', (req, res) => {
+  const userInfo = req.body;
 
-  console.log('hubInformation', hubInformation);
+  console.log('userInfo', userInfo);
 
-  db.add(hubInformation)
-    .then(hub => {
-      res.status(201).json(hub);
+  db.add(userInfo)
+    .then(user => {
+      res.status(201).json(user);
     })
     .catch(err => {
       console.log('error', err);
       res.status(500).json({
-        error: 'failed to ADD the hub to the db'
+        error: 'failed to ADD user to the db'
       });
     });
 });
 
 // DELETE a hub
-server.delete('/hubs/:id', (req, res) => {
+server.delete('/users/:id', (req, res) => {
   const id = req.params.id;
 
   db.remove(id)
     .then(count => {
-      res.status(200).json({ message: `hub with id ${id} deleted` });
+      res.status(200).json({ message: `user with id ${id} deleted` });
     })
     .catch(err => {
       console.log('error', err);
       res.status(500).json({
-        error: 'failed to DELETE hub from db'
+        error: 'failed to DELETE user from db'
       });
     });
 });
